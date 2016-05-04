d7566942f159b9999712b2baaf1afc7c9f2eb741
Kenny Victoria <kvict3@gmail.com>
2016-05-04 15:48:38 -0400

    added end button that hides quiz

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,75 +1,3 @@
-// //object block question and answer
-// var trivia = [
-// 	{number: 0, question: "Where is the highest point in the world?", answer: "Mount Chimborazo"},
-// 	{number: 1, question: "In what country can you weigh less, just by living there?", answer: "Canada"},
-// 	{number: 2, question: "Hurg de gurge in flibben flabben?", answer: "Foobar"},
-// 	{number: 3, question: "How are you feeling?", answer: "Great"}
-// ]
-
-// //object block stores other answers
-// var notAnswers = [
-// 	["Mount Kilimanjaro", "Mount Everest", "Mount McKinley"],
-// 	["Europe", "Chile", "Japan"],
-// 	["Bass", "Heliotrope", "Teppanyaki"],
-// 	["Tired", "Bad", "Malcontent"]
-// ]
-
-// //time conversion mechanic
-// function timeConverter (t) {
-//     var minutes = Math.floor(t/60);
-//     var seconds = t - (minutes * 60);
-//     if (seconds < 10){
-//       seconds = "0" + seconds;
-//     }
-//     if (minutes === 0){
-//       minutes = "00";
-//     } else if (minutes < 10){
-//       minutes = "0" + minutes;
-//     }
-//     return minutes + ":" + seconds;
-//  }
-// //timer for questionaire
-// $(document).ready(function(){
-// 	setTimeout(function(){
-// 	},1000);
-// 	time = 10
-// 	$("#timer").html("05:00");
-// 	function countDown(){
-// 		time--;
-// 		currentTime = timeConverter(time);
-// 		$("#timer").html(currentTime);
-// 		console.log(time);
-// 		//Time's Up mechanic
-// 		if (time == 0) {
-// 			for (var e=0; e<trivia.length; e++){
-// 				if ($('#question'+e+':checked').val() == trivia[e].answer){
-// 					correctAnswers++;
-// 				} else if ($('#question'+e+':checked').val() == undefined){
-// 					notAnswered++;
-// 				} else {
-// 					incorrectAnswers++;
-// 				}
-// 			}
-// 			$('#main-section').empty();
-// 			$('#main-section').append("<h1>Time\'s Up!</h1>");
-// 			$('#main-section').append("<p>You got "+correctAnswers+" correct answers!</p>");
-// 			$('#main-section').append("<p>You got "+incorrectAnswers+" incorrect answers.</p>");
-// 			$('#main-section').append("<p>You got "+notAnswered+" unanswered answers.</p>");
-// 		}
-// 	} 
-// 	counter = setInterval(countDown,1000);
-// })
-
-// //stores player selections into array
-// var correctAnswers = 0;
-// var incorrectAnswers = 0;
-// var notAnswered = 0;
-
-// // checks player answers against correct answers
-// 	for (var a=0; a<trivia.length; a++) {
-// 		if ($("name="+"question"+a)){}
-// 	}
-
 window.onload = function () {
     var display = document.querySelector('#time'),
         timer = new CountDownTimer(60),
@@ -89,13 +17,12 @@ window.onload = function () {
         display.textContent = minutes + ':' + seconds;
     }
 };
+
 $(document).ready(function(){
     $("#show").click(function(){
         $(".container").show();
     })
     });
-
-
 
 $('.answers input[type="radio"]').click(function(){
     
@@ -109,7 +36,7 @@ $('.answers input[type="radio"]').click(function(){
     $('.question').each(function(k, v){
         
         if(!$(v).find('input[type="radio"]:checked').length){
-            //question not answered
+           
             unanswered.push(v);
             return;
         }
@@ -126,8 +53,8 @@ $('.answers input[type="radio"]').click(function(){
         
     });
     $(this).closest('.answers').find('[type="radio"]').prop({ disabled: true });
-    console.log(unanswered.length, correct, incorrect);
 });
+
 
 function CountDownTimer(duration, granularity) {
   this.duration = duration;
@@ -181,5 +108,9 @@ CountDownTimer.parse = function(seconds) {
 };
 
 
+    $("#hide").click(function(){
+        $(".container").hide();
+        $(".timeline").hide();
+    })
 
 
